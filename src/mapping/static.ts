0c641ec42ed88a2a6fcb58d09e16fb1d24bd5ae0
Brian Beversdorf <bbeversdorf@gowithfloat.com>
2021-11-19 10:22:06 -0600

    Fixed Mapping Empty Static Keyed Items

--- src/mapping/static.ts
+++ src/mapping/static.ts
@@ -2,12 +2,16 @@ import { MappingInterface, MappedItem, MapSourceKey, BaseMapping } from './inter
 
 
 export class StaticMapping extends BaseMapping implements MappingInterface {
-  getValue(sourceKey: MapSourceKey, sourceItem: MappedItem): string[]|string {
+  getValue(sourceKey: MapSourceKey, sourceItem: MappedItem): string[]|string|undefined {
     if (!sourceKey.key) {
       return sourceKey.value as string;
     }
 
-    const keys = this.getValueFromKey(sourceKey.key as string, sourceItem) as string;
+    const keys = this.getValueFromKey(sourceKey.key as string, sourceItem);
+    if (!keys) {
+      return;
+    }
+
     if (Array.isArray(keys)) {
       return keys.map(() => sourceKey.value as string);
     }
